2bdfde42fb1119fc2d773993b87336c609a8ce84
GareevTF <gareevtf@gmail.com>
2020-03-21 02:44:51 +0500

    Menu button added

--- app/src/main/java/com/example/perevodchik/MainActivity.java
+++ app/src/main/java/com/example/perevodchik/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.perevodchik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.perevodchik.renderer.MockWordProvider;
 import com.example.perevodchik.renderer.Translate;
@@ -12,6 +17,8 @@ import com.example.perevodchik.renderer.TranslateRenderer;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.perevodchik.R.menu.menu_main;
 
 public class MainActivity extends AppCompatActivity
         implements TranslateRenderer.TranslateListener {
@@ -21,8 +28,31 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initRecycler();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_new_word: {
+                Intent intent = new Intent(this, AddWordActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            case R.id.action_change_word: {
+                Intent intent = new Intent(this, ChangeWordActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initRecycler() {
